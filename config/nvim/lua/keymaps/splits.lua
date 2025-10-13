211e8f398631ffedcf5922e86a484702d03265ea
Fulvia Mourikis <zum281@gmail.com>
2025-10-13 13:23:02 +0200

    fix: splits keymaps nvim

--- config/nvim/lua/keymaps/splits.lua
+++ config/nvim/lua/keymaps/splits.lua
@@ -3,5 +3,5 @@ local wk = require("which-key")
 wk.add({
 	{ "<leader>s", group = "split", remap = false, nowait = true },
 	{ "<leader>sv", ":vsplit<CR>", desc = "Split window vertically" },
-	{ "<leader>sh", ":split<CR", desc = "Split window horizontally" },
+	{ "<leader>sh", ":split<CR>", desc = "Split window horizontally" },
 })
