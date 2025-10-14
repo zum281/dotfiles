9e9773ee47a75f443a526600c1a22968cd935787
Fulvia Mourikis <zum281@gmail.com>
2025-10-14 22:22:12 +0200

    nvim autocmd organize imports

--- config/nvim/lua/autocmd.lua
+++ config/nvim/lua/autocmd.lua
@@ -1,12 +1,4 @@
 local augroup = vim.api.nvim_create_augroup("UserConfig", {})
--- format on save
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
-})
 
 -- Remove trailing space
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
@@ -19,5 +11,58 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = augroup,
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+-- organize imports on save
+local function organize_imports(bufnr)
+	local filetype = vim.bo.filetype
+
+	if filetype:match("^javascript") or filetype:match("^typescript") then
+		local clients = vim.lsp.get_clients({ bufnr = bufnr })
+		local ts_client = nil
+
+		for _, client in pairs(clients) do
+			if client.name == "tsserver" or client.name == "ts_ls" or client.name == "vtsls" then
+				ts_client = client
+				break
+			end
+		end
+
+		if ts_client then
+			local params = {
+				command = "_typescript.organize_imports",
+				arguments = { vim.api.nvim_buf_get_name(bufnr) },
+			}
+			local success = vim.lsp.buf_request_sync(bufnr, "workspace/executeCommand", params, 1000)
+			if not success then
+				vim.lsp.bug.code_action({
+					bufnr = bufnr,
+					context = {
+						only = { "source.organizeImports" },
+						apply = true,
+						timeout = 500,
+					},
+				})
+			end
+		end
+	end
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+	group = vim.api.nvim_create_augroup("TSSaveActions", { clear = true }),
+	callback = function(args)
+		organize_imports(args.buf)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+-- format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*", "!*.ts", "!*.tsx", "!*.js", "!*.jsx" },
+	group = vim.api.nvim_create_augroup("GeneralFormatOnSave", { clear = true }),
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
 	end,
 })
