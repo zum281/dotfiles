e12afb5470e1ccb3dc33642f03ed75ffed04e776
Fulvia Mourikis <zum281@gmail.com>
2025-10-15 18:26:30 +0200

    nvim update colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,13 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("moonfly").custom_colors({
+			black = "#000000",
+			bg = "#000000",
+			grey0 = "#4d5d8d",
+			grey11 = "#000000",
+			grey16 = "#080808",
+		})
 		vim.g.moonflyCursorColor = true
 		vim.o.winborder = "single"
 		vim.g.moonflyNormalFloat = true
