22528a3edf3e201a9e29fdcc61feecbc971f7c49
Fulvia Mourikis <zum281@gmail.com>
2025-11-26 18:02:38 +0100

    nvim: init for vscode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,11 @@
-require("core.opts")
-require("config.lazy")
-require("core.lsp")
-require("core.keymaps")
-require("core.autocmd")
-require("core.colorscheme")
-
+if vim.g.vscode then
+	require("core.opts")
+	require("core.keymaps")
+else
+	require("core.opts")
+	require("config.lazy")
+	require("core.lsp")
+	require("core.keymaps")
+	require("core.autocmd")
+	require("core.colorscheme")
+end
