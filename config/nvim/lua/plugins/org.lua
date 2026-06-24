458ca38e2b87c8f5a905d6c573d90824e88ba0cc
Fulvia Mourikis <zum281@gmail.com>
2026-06-24 22:51:54 +0200

    nvim: org add PROGRESS status

--- config/nvim/lua/plugins/org.lua
+++ config/nvim/lua/plugins/org.lua
@@ -1,6 +1,7 @@
 require("orgmode").setup({
 	org_agenda_files = "~/org/**/*.org",
 	org_default_notes_file = "~/org/refile.org",
+	org_todo_keywords = { "TODO", "PROGRESS", "|", "DONE" },
 	org_capture_templates = {
 		t = {
 			description = "Task",
