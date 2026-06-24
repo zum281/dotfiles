e4b22982406a2839bc2ae8bc797e4945169e02a2
Fulvia Mourikis <zum281@gmail.com>
2026-06-24 21:13:42 +0200

    nvim: org todo template

--- config/nvim/lua/plugins/org.lua
+++ config/nvim/lua/plugins/org.lua
@@ -1,6 +1,13 @@
 require("orgmode").setup({
 	org_agenda_files = "~/org/**/*.org",
 	org_default_notes_file = "~/org/refile.org",
+	org_capture_templates = {
+		t = {
+			description = "Task",
+			template = "* TODO [#%^{Priority|A|B|C}] %^{Title} :%^{Project}:\n%?",
+			target = "~/org/refile.org",
+		},
+	},
 	mappings = {
 		global = {
 			org_agenda = false,
