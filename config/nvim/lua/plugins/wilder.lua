c8ecbb52a1cb939fcc71bfc5914ca151c79a4095
Fulvia Mourikis <zum281@gmail.com>
2026-06-25 18:26:40 +0200

    nvim: update wilder find

--- config/nvim/lua/plugins/wilder.lua
+++ config/nvim/lua/plugins/wilder.lua
@@ -6,8 +6,21 @@ wilder.setup({
 	previous_key = "<S-Tab>",
 })
 
+local function is_find_arg(cmdline)
+	local cmd = cmdline:match("^%s*(%a+)%s")
+	return cmd ~= nil and #cmd >= 3 and ("find"):find("^" .. vim.pesc(cmd)) ~= nil
+end
+
 wilder.set_option("pipeline", {
-	wilder.branch(wilder.cmdline_pipeline({ fuzzy = 1 }), wilder.search_pipeline()),
+	wilder.branch({
+		wilder.check(function(_, x)
+			return is_find_arg(x)
+		end),
+		function(_, x)
+			return vim.fn.getcompletion(x, "cmdline")
+		end,
+		wilder.result({ replace = { "wilder#cmdline#replace" } }),
+	}, wilder.cmdline_pipeline({ fuzzy = 1 }), wilder.search_pipeline()),
 })
 
 local pal = require("zusk.palette")
