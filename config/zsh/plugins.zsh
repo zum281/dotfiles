63b249d5006713cd48926c80912a259a0c554ce7
Fulvia Mourikis <zum281@gmail.com>
2026-06-26 23:04:14 +0200

    zsh: restore fast-syntax-highlighting (dropped in modularize)

--- config/zsh/plugins.zsh
+++ config/zsh/plugins.zsh
@@ -7,6 +7,7 @@ zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
 zinit ice depth=1
 zinit light jeffreytse/zsh-vi-mode
+zinit light zdharma-continuum/fast-syntax-highlighting
 
 function zvm_after_init() {
 	bindkey '^R' fzf-history-widget
