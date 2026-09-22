0e2eb88716d308a61f07ff746084214cf0afb51e
Fulvia Mourikis <zum281@gmail.com>
2026-09-22 14:13:07 +0200

    nvim: lsp

--- config/nvim/lua/core/lsp.lua
+++ config/nvim/lua/core/lsp.lua
@@ -1,45 +1,56 @@
 vim.lsp.inlay_hint.enable(true)
 
+vim.filetype.add({
+	filename = {
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+	},
+})
+
 vim.lsp.enable({
-  "bash_ls",
-  "clang_ls",
-  "lua_ls",
-  "ts_ls",
-  "emmet_ls",
-  "css_ls",
-  "css_vars_ls",
-  "tailwind_ls",
-  "yaml_ls",
-  "go_ls",
-  "sql_ls",
-  "pyright_ls",
-  "ruff_ls",
-  "jdtls_ls",
-  "racket_ls",
-  "rust_analyzer",
+	"bash_ls",
+	"clang_ls",
+	"lua_ls",
+	"ts_ls",
+	"emmet_ls",
+	"css_ls",
+	"css_vars_ls",
+	"tailwind_ls",
+	"yaml_ls",
+	"go_ls",
+	"sql_ls",
+	"pyright_ls",
+	"ruff_ls",
+	"jdtls_ls",
+	"racket_ls",
+	"rust_analyzer",
+	"docker_ls",
+	"docker_compose_ls",
 })
 
 vim.diagnostic.config({
-  virtual_lines = { current_line = true },
+	virtual_lines = { current_line = true },
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    local provider = client and client.server_capabilities.completionProvider
-    if not provider then
-      return
-    end
-    -- autotrigger only fires on triggerCharacters; add word chars to complete as you type
-    local chars = provider.triggerCharacters or {}
-    for c = ("a"):byte(), ("z"):byte() do
-      chars[#chars + 1] = string.char(c)
-    end
-    for c = ("A"):byte(), ("Z"):byte() do
-      chars[#chars + 1] = string.char(c)
-    end
-    chars[#chars + 1] = "_"
-    provider.triggerCharacters = chars
-    vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-  end,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local provider = client and client.server_capabilities.completionProvider
+		if not provider then
+			return
+		end
+		-- autotrigger only fires on triggerCharacters; add word chars to complete as you type
+		local chars = provider.triggerCharacters or {}
+		for c = ("a"):byte(), ("z"):byte() do
+			chars[#chars + 1] = string.char(c)
+		end
+		for c = ("A"):byte(), ("Z"):byte() do
+			chars[#chars + 1] = string.char(c)
+		end
+		chars[#chars + 1] = "_"
+		provider.triggerCharacters = chars
+		vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+	end,
 })
